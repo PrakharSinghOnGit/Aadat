e22259563dac6f821a28dbae7c5ea08574633b4f
Prakhar Singh <shaansinghd1209@gmail.com>
2026-04-12 22:49:57 +0530

    Initial Commit

---
+++ Aadat/AadatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AadatApp.swift
+//  Aadat
+//
+//  Created by Shaan SIngh on 12/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AadatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
